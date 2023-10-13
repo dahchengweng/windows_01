d0d69a3174270c71e42219f542ce10d9e2f100dd
Dah Cheng Weng <dah1214@gmail.com>
2023-10-14 03:58:21 +0800

    Window Programming lesson 01
    This Program is created by visual studio 2022, to understand how to view resource file, modify them, and how windows program work by Call RegisterClass, CreateWindow, and setup Window Procedure to receive
    the Windows Message from OS.

---
+++ HelloWindows.cpp
@@ -0,0 +1,263 @@
+﻿// HelloWindows.cpp : 定義應用程式的進入點。
+//
+
+#include "framework.h"
+#include "HelloWindows.h"
+#include "resource.h"
+
+#define MAX_LOADSTRING 100
+
+// 全域變數:
+HINSTANCE hInst;                                // 目前執行個體
+WCHAR szTitle[MAX_LOADSTRING];                  // 標題列文字
+WCHAR szWindowClass[MAX_LOADSTRING];            // 主視窗類別名稱
+
+// 這個程式碼模組所包含之函式的向前宣告:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 在此放置程式碼。
+
+    // 將全域字串初始化
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_HELLOWINDOWS, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 執行應用程式初始化:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_HELLOWINDOWS));
+
+    MSG msg;
+
+    // 主訊息迴圈:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  函式: MyRegisterClass()
+//
+//  用途: 註冊視窗類別。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_HELLOWINDOWS));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_HELLOWINDOWS);
+    //wcex.lpszMenuName   = NULL;
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   函式: InitInstance(HINSTANCE, int)
+//
+//   用途: 儲存執行個體控制代碼並且建立主視窗
+//
+//   註解:
+//
+//        在這個函式中，我們將執行個體控制代碼儲存在全域變數中，
+//        並建立及顯示主程式視窗。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 將執行個體控制代碼儲存在全域變數中
+
+   HWND hWnd = CreateWindowW(szWindowClass, L"Hello World", WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+int ShowMessagBox()
+{
+    int msgboxID = MessageBox(
+        NULL,
+        (LPCWSTR)L"My First MessageBox",
+        (LPCWSTR)L"MessageBox title",
+        MB_OKCANCEL
+    );
+
+    switch (msgboxID)
+    {
+    case IDCANCEL:
+        // TODO: add code
+        OutputDebugStringW(L"You Press Cancel.");
+        break;
+    case IDOK:
+        // TODO: add code
+        OutputDebugStringW(L"You Press OK.");
+        break;
+    }
+
+    return msgboxID;
+}
+
+
+
+void DisplayMyNAMessageBox() {
+    int msgboxID = MessageBox(
+        NULL,
+        (LPCWSTR)L"This is my custom message box",
+        (LPCWSTR)L"MessageBox Exercise",
+        MB_OK
+    );
+
+    switch (msgboxID)
+    {
+        case IDOK:
+            OutputDebugStringW(L"You Press OK.");
+            break;
+        default:
+            break;
+    }
+}
+
+
+int DisplayResourceNAMessageBox()
+{
+    int msgboxID = MessageBox(
+        NULL,
+        (LPCWSTR)L"Resource not available\nDo you want to try again?",
+        (LPCWSTR)L"Account Details",
+        MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2
+    );
+
+    switch (msgboxID)
+    {
+    case IDCANCEL:
+        // TODO: add code
+        break;
+    case IDTRYAGAIN:
+        // TODO: add code
+        break;
+    case IDCONTINUE:
+        // TODO: add code
+        break;
+    }
+
+    return msgboxID;
+}
+
+//
+//  函式: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  用途: 處理主視窗的訊息。
+//
+//  WM_COMMAND  - 處理應用程式功能表
+//  WM_PAINT    - 繪製主視窗
+//  WM_DESTROY  - 張貼結束訊息然後傳回
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 剖析功能表選取項目:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                ShowMessagBox();
+                OutputDebugStringW(L"IDM_EXIT ******");
+                //DestroyWindow(hWnd);
+                break;
+            case ID_32775:
+                OutputDebugStringW(L"ID_32775 is Clicked...");
+                break;
+            case ID_TEST_MENU:
+                DisplayMyNAMessageBox();
+                    //DisplayResourceNAMessageBox();
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: 在此新增任何使用 hdc 的繪圖程式碼...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+
+        OutputDebugStringW(L"WM DESTROY 000000");
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// [關於] 方塊的訊息處理常式。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
